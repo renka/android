eec82b2815cb12b0b5059b2ebeeadbfc62fb8fea
Iren Saturina <sin0988@gmail.com>
2015-11-26 10:30:09 +0200

    Open built-in apps

--- app/src/main/java/com/example/isaturina/training/ActivityA.java
+++ app/src/main/java/com/example/isaturina/training/ActivityA.java
@@ -1,7 +1,13 @@
 package com.example.isaturina.training;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class ActivityA extends AppCompatActivity {
 
@@ -10,4 +16,41 @@ public class ActivityA extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
     }
+
+    public void methodA(View view) {
+        Uri number = Uri.parse("tel:5551234");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(callIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+        if (isIntentSafe) {
+            startActivity(callIntent);
+        }
+    }
+
+    public void methodB(View view) {
+        Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(mapIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        if ( activities.size() > 0) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void methodC(View view) {
+        Uri webpage = Uri.parse("http://www.android.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(webIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        if ( activities.size() > 0) {
+            startActivity(webIntent);
+        }
+
+    }
+
 }
