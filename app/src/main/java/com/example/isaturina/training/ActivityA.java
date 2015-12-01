57675fffa300a43678bd80ee67d09f13ba5cad83
Iren Saturina <sin0988@gmail.com>
2015-12-01 21:13:57 +0200

    Location history & updates

--- app/src/main/java/com/example/isaturina/training/ActivityA.java
+++ app/src/main/java/com/example/isaturina/training/ActivityA.java
@@ -1,13 +1,164 @@
 package com.example.isaturina.training;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ActivityA extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+
+public class ActivityA extends Activity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private TextView latitude;
+    private TextView longitude;
+    private TextView choice;
+    private CheckBox fineAcc;
+    private Button choose;
+    private TextView provText;
+    private LocationManager locationManager;
+    private String provider;
+    private MyLocationListener mylistener;
+    private Criteria criteria;
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Called when the activity is first created.
+     */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        latitude = (TextView) findViewById(R.id.lat);
+        longitude = (TextView) findViewById(R.id.lon);
+        provText = (TextView) findViewById(R.id.prov);
+        choice = (TextView) findViewById(R.id.choice);
+        fineAcc = (CheckBox) findViewById(R.id.fineAccuracy);
+        choose = (Button) findViewById(R.id.chooseRadio);
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);    //default
+
+        // user defines the criteria
+        choose.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (fineAcc.isChecked()) {
+                    criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                    choice.setText("fine accuracy selected");
+                } else {
+                    criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+                    choice.setText("coarse accuracy selected");
+                }
+            }
+        });
+        criteria.setCostAllowed(false);
+        // get the best provider depending on the criteria
+        provider = "network";//locationManager.getBestProvider(criteria, false);
+
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient); //locationManager.getLastKnownLocation(provider);
+        mylistener = new MyLocationListener();
+
+//        latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+//        longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+//        provText.setText(provider + " provider has been selected.");
+
+        if (location != null) {
+            mylistener.onLocationChanged(location);
+        } else {
+            // leads to the settings because there is no last known location
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+//         location updates: at least 1 meter and 200millsecs change
+        locationManager.requestLocationUpdates(provider, 200, 1, mylistener);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // Initialize the location fields
+            latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+            longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+            provText.setText(provider + " provider has been selected.");
+
+            Toast.makeText(ActivityA.this, "Location changed!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Toast.makeText(ActivityA.this, provider + "'s status changed to " + status + "!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(ActivityA.this, "Provider " + provider + " enabled!",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(ActivityA.this, "Provider " + provider + " disabled!",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
