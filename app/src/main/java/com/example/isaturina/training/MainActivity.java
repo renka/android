b691e928b02ccbacfb62a85a887c6f620665a9f2
Iren Saturina <isaturina@thisiszone.com>
2015-12-15 16:51:02 +0200

    Touch gestures app

--- app/src/main/java/com/example/isaturina/training/MainActivity.java
+++ app/src/main/java/com/example/isaturina/training/MainActivity.java
@@ -1,54 +1,70 @@
-/* Copyright (C) 2012 The Android Open Source Project
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+/*
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.isaturina.training;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-
-import com.example.isaturina.training.charting.PieChart;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    private InteractiveLineGraphView mGraphView;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGraphView = (InteractiveLineGraphView) findViewById(R.id.chart);
+    }
 
-        Resources res = getResources();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-        setContentView(R.layout.main);
-        final PieChart pie = (PieChart) this.findViewById(R.id.Pie);
-        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
-        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
-        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
-        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
-        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
-        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
-        pie.addItem("Piu", 3, res.getColor(R.color.my));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_zoom_in:
+                mGraphView.zoomIn();
+                return true;
 
-        ((Button) findViewById(R.id.Reset)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                pie.setCurrentItem(0);
-            }
-        });
+            case R.id.action_zoom_out:
+                mGraphView.zoomOut();
+                return true;
+
+            case R.id.action_pan_left:
+                mGraphView.panLeft();
+                return true;
+
+            case R.id.action_pan_right:
+                mGraphView.panRight();
+                return true;
+
+            case R.id.action_pan_up:
+                mGraphView.panUp();
+                return true;
+
+            case R.id.action_pan_down:
+                mGraphView.panDown();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
-
